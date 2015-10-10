3a46945501fc3f7c73fd7939e4f7810d298bae8c
NanaEB <nebal.elbebbili@haw-hamburg.de>
2015-10-10 14:15:10 +0200

    added not- editor

---
+++ not.combined.workspace/org.kermeta.fsm.gmf.diagram/src/fsm/diagram/edit/parts/FsmEditPartFactory.java
@@ -0,0 +1,102 @@
+package fsm.diagram.edit.parts;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPartFactory;
+import org.eclipse.gef.tools.CellEditorLocator;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ITextAwareEditPart;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.gmf.tooling.runtime.directedit.locator.CellEditorLocatorAccess;
+
+import fsm.diagram.part.FsmVisualIDRegistry;
+
+/**
+ * @generated
+ */
+public class FsmEditPartFactory implements EditPartFactory {
+
+	/**
+	 * @generated
+	 */
+	public EditPart createEditPart(EditPart context, Object model) {
+		if (model instanceof View) {
+			View view = (View) model;
+			switch (FsmVisualIDRegistry.getVisualID(view)) {
+
+			case FSMEditPart.VISUAL_ID:
+				return new FSMEditPart(view);
+
+			case SteadyStateEditPart.VISUAL_ID:
+				return new SteadyStateEditPart(view);
+
+			case SteadyStateNameEditPart.VISUAL_ID:
+				return new SteadyStateNameEditPart(view);
+
+			case TransientStateEditPart.VISUAL_ID:
+				return new TransientStateEditPart(view);
+
+			case TransientStateNameEditPart.VISUAL_ID:
+				return new TransientStateNameEditPart(view);
+
+			case InitialStateEditPart.VISUAL_ID:
+				return new InitialStateEditPart(view);
+
+			case ActionEditPart.VISUAL_ID:
+				return new ActionEditPart(view);
+
+			case ActionEntryLabelEditPart.VISUAL_ID:
+				return new ActionEntryLabelEditPart(view);
+
+			case EActionEditPart.VISUAL_ID:
+				return new EActionEditPart(view);
+
+			case EActionExitLabelEditPart.VISUAL_ID:
+				return new EActionExitLabelEditPart(view);
+
+			case Action2EditPart.VISUAL_ID:
+				return new Action2EditPart(view);
+
+			case ActionEntryLabel2EditPart.VISUAL_ID:
+				return new ActionEntryLabel2EditPart(view);
+
+			case EAction2EditPart.VISUAL_ID:
+				return new EAction2EditPart(view);
+
+			case EActionExitLabel2EditPart.VISUAL_ID:
+				return new EActionExitLabel2EditPart(view);
+
+			case SteadyStateSteady_entry_exit_actionsEditPart.VISUAL_ID:
+				return new SteadyStateSteady_entry_exit_actionsEditPart(view);
+
+			case TransientStateTransient_entry_exit_actionsEditPart.VISUAL_ID:
+				return new TransientStateTransient_entry_exit_actionsEditPart(
+						view);
+
+			case TransitionEditPart.VISUAL_ID:
+				return new TransitionEditPart(view);
+
+			case TransitionActionGuardEditPart.VISUAL_ID:
+				return new TransitionActionGuardEditPart(view);
+
+			}
+		}
+		return createUnrecognizedEditPart(context, model);
+	}
+
+	/**
+	 * @generated
+	 */
+	private EditPart createUnrecognizedEditPart(EditPart context, Object model) {
+		// Handle creation of unrecognized child node EditParts here
+		return null;
+	}
+
+	/**
+	 * @generated
+	 */
+	public static CellEditorLocator getTextCellEditorLocator(
+			ITextAwareEditPart source) {
+		return CellEditorLocatorAccess.INSTANCE
+				.getTextCellEditorLocator(source);
+	}
+
+}
---
+++ not.combined.workspace/org.kermeta.fsm.gmf/src/fsm/impl/ActionImpl.java
@@ -0,0 +1,163 @@
+/**
+ */
+package fsm.impl;
+
+import fsm.Action;
+import fsm.FsmPackage;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Action</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link fsm.impl.ActionImpl#getEntryLabel <em>Entry Label</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ActionImpl extends MinimalEObjectImpl.Container implements Action {
+	/**
+	 * The default value of the '{@link #getEntryLabel() <em>Entry Label</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getEntryLabel()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String ENTRY_LABEL_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getEntryLabel() <em>Entry Label</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getEntryLabel()
+	 * @generated
+	 * @ordered
+	 */
+	protected String entryLabel = ENTRY_LABEL_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ActionImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return FsmPackage.Literals.ACTION;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getEntryLabel() {
+		return entryLabel;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setEntryLabel(String newEntryLabel) {
+		String oldEntryLabel = entryLabel;
+		entryLabel = newEntryLabel;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, FsmPackage.ACTION__ENTRY_LABEL, oldEntryLabel, entryLabel));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case FsmPackage.ACTION__ENTRY_LABEL:
+				return getEntryLabel();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case FsmPackage.ACTION__ENTRY_LABEL:
+				setEntryLabel((String)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case FsmPackage.ACTION__ENTRY_LABEL:
+				setEntryLabel(ENTRY_LABEL_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case FsmPackage.ACTION__ENTRY_LABEL:
+				return ENTRY_LABEL_EDEFAULT == null ? entryLabel != null : !ENTRY_LABEL_EDEFAULT.equals(entryLabel);
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (entryLabel: ");
+		result.append(entryLabel);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ActionImpl
